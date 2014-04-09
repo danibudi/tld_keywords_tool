c668dd4bed0c94fef802437b7d3acc26c68f053c
Dani <dani.budinova@spreadpointe.com>
2014-04-09 18:21:45 +0300

    amount_of_domains_checked_in_one_API_call in local_settings.py tests

--- kwt/namecheap_api.py
+++ kwt/namecheap_api.py
@@ -3,7 +3,8 @@ import urllib
 import urllib2
 import xml.etree.ElementTree as ET
 
-from settings import api_key, api_user, command, user_name, clientIp, url
+from settings import (api_key, api_user, command, user_name, clientIp, url,
+                      amount_of_domains_checked_in_one_API_call)
 
 
 def parser_data(text=""):
@@ -28,33 +29,42 @@ def parser_data(text=""):
 
 
 def namecheap_domains_check(domain_list=[]):
+    """ the exact limit of the amount of the domains that
+    can be checked in one API call is 120
+    there's a limitation on the string length for GET -
+    the length of a URL is limited to 2048 characters maximum.
+    https://community.namecheap.com/forums/viewtopic.php?f=17&t=7568"""
     domains_status = {}
     err = ''
     if not domain_list:
         return domains_status, err
-    domains_idna = ','.join(dom.encode('idna') for dom in domain_list)
     data = {}
-    data['DomainList'] = domains_idna
     data['ApiUser'] = api_user
     data['UserName'] = user_name
     data['ApiKey'] = api_key
     data['Command'] = command
     data['ClientIp'] = clientIp
-    url_values = urllib.urlencode(data)
-    full_url = url + '?' + url_values
-    data = urllib2.urlopen(full_url)
-    the_page = data.read()
-    root = ET.fromstring(the_page)
-    if root.iter('Errors'):
-        for e in root.iter('{http://api.namecheap.com/xml.response}Error'):
-            try:
-                err = err + e.text + '; '  # .split("Provider for tld '")[1][:3].strip("'")
-            except:
-                err_c = e.text
-                if (domains_status != {}) and err_c == 'Parameter DomainList is Missing':
-                    err = err + err_c + '; '
-    for domain in root.iter(
-        '{http://api.namecheap.com/xml.response}DomainCheckResult'):
-        b = (domain.attrib['Available'] == 'true')
-        domains_status[unicode(domain.attrib['Domain'], 'idna')] = b
+    domainList = [dom.encode('idna') for dom in domain_list]
+    max_amount_domains = amount_of_domains_checked_in_one_API_call
+    for i, d in enumerate(range(len(domainList)/max_amount_domains+1)):
+        elements_for_namecheap = domainList[i*max_amount_domains:(d+1)*max_amount_domains]
+        domains = ','.join(i for i in elements_for_namecheap)
+        data['DomainList'] = domains
+        url_values = urllib.urlencode(data)
+        full_url = url + '?' + url_values
+        data_url = urllib2.urlopen(full_url)
+        the_page = data_url.read()
+        root = ET.fromstring(the_page)
+        if root.iter('Errors'):
+            for e in root.iter('{http://api.namecheap.com/xml.response}Error'):
+                try:
+                    err = err + e.text + '; '  # .split("Provider for tld '")[1][:3].strip("'")
+                except:
+                    err_c = e.text
+                    if (domains_status != {}) and err_c == 'Parameter DomainList is Missing':
+                        err = err + err_c + '; '
+        for domain in root.iter(
+            '{http://api.namecheap.com/xml.response}DomainCheckResult'):
+            b = (domain.attrib['Available'] == 'true')
+            domains_status[unicode(domain.attrib['Domain'], 'idna')] = b
     return (domains_status, err)
--- kwt/test_namecheap.py
+++ kwt/test_namecheap.py
@@ -6,19 +6,34 @@ import unittest
 from namecheap_api import namecheap_domains_check, parser_data
 
 domains = [u'alabaladomain.com', u'раббота.com', u'труд.com',
-           u'availabledomain.com', u'google.com']
+           u'availabledomain.com', u'google.com', 'vintech.bg']
 
 
 class TestApi(unittest.TestCase):
     def test_get_HTTPResponce(self):
         domains_status, err = namecheap_domains_check(domain_list=domains)
         self.assertEquals(domains_status,
-                          {'google.com': False,  # 'vintech.com': 'false',
+                          {'google.com': False,
+                           # 'vintech.bg': Provider for tld 'bg' is not found;
                            'availabledomain.com': False, u'раббота.com': True,
                            u'труд.com': False, 'alabaladomain.com': True})
+
+    def test_get_HTTPResponce_over_NC_limit(self):
+        domains_status, err = namecheap_domains_check(
+            domain_list=domains*10 + ['conzap.com'])
+
+        self.assertEquals(domains_status,
+                          {'google.com': False, 'conzap.com': False,
+                           u'раббота.com': True,
+                           'availabledomain.com': False,
+                           u'труд.com': False, 'alabaladomain.com': True})
+
+    def test_get_HTTPResponce_no_domain_list(self):
         domains_status, err = namecheap_domains_check()
         self.assertEquals(domains_status, {})
-        domains_status, err = namecheap_domains_check('dummy')
+
+    def test_get_HTTPResponce_dummy_domain_list(self):
+        domains_status, err = namecheap_domains_check(['dummy'])
         self.assertEquals(domains_status, {})
 
     def test_parse(self):
@@ -36,7 +51,7 @@ class TestApi(unittest.TestCase):
         self.assertDictEqual(kw_sv_dict,
                              {'look': '11', 'similar': '12',
                               'ttqt': 'asd', 'ttt': '34',
-                              'website': '15', 'when': '13','your': '14'})
+                              'website': '15', 'when': '13', 'your': '14'})
 
 
 if __name__ == '__main__':
